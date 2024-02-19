667166a03d39eb03d0312492900f7c3b8a985559
philiplbrown <philiplbrown022@gmail.com>
2024-02-19 10:41:36 +0600

    added for

---
+++ for.js
@@ -0,0 +1,14 @@
+// Get full number
+for(var i =1; i <= 20; i++){
+    console.log(i);
+}
+
+// Get even number by for loop
+for(var i = 0; i <= 30; i+=2){
+    console.log(i)
+}
+
+// Get odd number by for loop
+for(var i = 1; i <30; i+=2){
+    console.log(i)
+}
