6e36ec0292db5def5c27577ffe743a92805b668d
philiplbrown <philiplbrown022@gmail.com>
2024-02-19 11:11:25 +0600

    added array-loops

---
+++ array-loops.js
@@ -0,0 +1,11 @@
+var numbers = ['55','88','99','25','68','358','586','33','47'];
+for(var i = 0; i < numbers.length; i++){
+    var number = numbers[i];
+    console.log(number)
+}
+
+var players =['Sakib', 'Liton', 'Mushi', 'Towhid', 'Sarkar', 'mahedi', 'Rony', 'Fiz','Towhid', 'Taskin', 'Saif'];
+for(var i= 0; i < players.length; i++){
+    var player = players[i];
+    console.log(player);
+}
