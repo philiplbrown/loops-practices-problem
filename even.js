0663bd28c6d7b3a3a9e18aaa1d723306e607a1eb
philiplbrown <philiplbrown022@gmail.com>
2024-02-19 10:17:18 +0600

    added number, even, odd, while

---
+++ even.js
@@ -0,0 +1,6 @@
+// TO get 0 to 30 all even numbers.
+var number = 0;
+while(number <= 30){
+    console.log(number);
+    number = number +2;
+}
---
+++ while.js
@@ -0,0 +1,13 @@
+var pizzaGiven = 0;
+while(pizzaGiven < 8) {
+    console.log('Pizza den, please11');
+    console.log(pizzaGiven);
+    pizzaGiven ++;
+}
+
+var kacchiGive = 0;
+while(kacchiGive < 9) {
+    console.log('Kacchi den, please');
+    console.log(kacchiGive);
+    kacchiGive ++;
+}
